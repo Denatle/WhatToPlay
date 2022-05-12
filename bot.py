5f810a20e0426c9fe212baf2665a9069a25fa6b2
Denatle <iq.labozin@gmail.com>
2022-05-12 19:39:47 +0300

    Polls

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext.commands import has_permissions
+from discord.ext.commands import has_permissions, cooldown, BucketType
 
 from modules.randomizer import Randomizer
 from modules.parser import Parser
@@ -29,7 +29,9 @@ async def gamesToPlay(ctx: discord.commands.context.ApplicationContext):
                    guild_ids=[910065668563533894])
 async def randomGameToPlay(ctx: discord.commands.context.ApplicationContext):
     games = await games_parser.Games(ctx.guild, asList=True)
-    await ctx.respond(await games_randomizer.randomGame(games), ephemeral=True)
+    game = await games_randomizer.randomGame(games)
+    await ctx.respond(game, ephemeral=True)
+
 
 @has_permissions(manage_roles=True)
 @bot.slash_command(name='create_roles',
@@ -40,5 +42,26 @@ async def createRolesByGames(ctx: discord.commands.context.ApplicationContext):
     await role_manager.createRolesByGames(games, ctx.guild)
     await ctx.respond("Roles created!", ephemeral=True)
 
+
+@cooldown(2, 30, BucketType.user)
+@bot.slash_command(name='poll',
+                   description="Starts poll for game",
+                   guild_ids=[910065668563533894])
+async def poll(ctx: discord.commands.context.ApplicationContext,
+               game_role: discord.Option(discord.Role, "Game role")):
+    games = await games_parser.Games(ctx.guild, asList=True)
+    if game_role.name in games:
+        embed = discord.Embed(
+            title='!POLL!',
+            description=f'Do you want to play {game_role.mention}?',
+            color=0xffffff)
+        await ctx.send(game_role.mention, embed=embed)
+    else:
+        await ctx.respond("Not a game role", ephemeral=True)
+
+
+@bot.event
+async def on_application_command_error(ctx, error):
+    await ctx.respond(error, ephemeral=True)
 
 bot.run("OTcxNzI3NTQ5NDEzNTkzMTI4.GW1RQu.VBrZctP9RicDuXbQvhI6oOrWfRhoQ3hYOuCN9Y")
